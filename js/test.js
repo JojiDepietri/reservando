94e7478536f0b8ebfe4a3dcbf3bd9e5d0cd887db
Joji Depietri <jojidepietri@Joji.local>
2019-08-30 17:34:32 -0300

    refactorizando

--- js/test.js
+++ js/test.js
@@ -2,58 +2,67 @@ const expect = chai.expect;
 
 describe('Restaurant', () => {
     describe('reservarHorario(horario)', () => {
-        it('Cuando se reserva un horario de un restaurant, el horario correspondiente se elimina del arreglo.', () => {
-            const nuevoRestaurant = new Restaurant(1, "TAO Uptown", "Asiática", "Nueva York", ["13:00", "15:30", "18:00"], "../img/asiatica1.jpg", [6, 7, 9, 10, 5]);
+        it('Si se reserva un horario de un restaurant, el horario se debe eliminar del arreglo horarios.', () => {
+            const nuevoRestaurant = new Restaurant();
+            nuevoRestaurant.horarios = ["13:00", "15:30", "18:00"];
             nuevoRestaurant.reservarHorario('13:00');
             expect(nuevoRestaurant.horarios).to.eql(['15:30','18:00']);
         })
 
-        it('Cuando se reserva un horario que el restaurant no posee, el arreglo se mantiene igual.', () => {
-            const nuevoRestaurant = new Restaurant(1, "TAO Uptown", "Asiática", "Nueva York", ["13:00", "15:30", "18:00"], "../img/asiatica1.jpg", [6, 7, 9, 10, 5]);
+        it('Si se reserva un horario que el restaurant no posee, el arreglo se mantiene igual.', () => {
+            const nuevoRestaurant = new Restaurant();
+            nuevoRestaurant.horarios = ["13:00", "15:30", "18:00"];
             nuevoRestaurant.reservarHorario('14:00');
             expect(nuevoRestaurant.horarios).to.eql(["13:00",'15:30','18:00']);
         })
 
-        it('Cuando se intenta reservar un horario pero no se le pasa ningún parámetro a la función, el arreglo se mantiene igual.', () => {
-            const nuevoRestaurant = new Restaurant(1, "TAO Uptown", "Asiática", "Nueva York", ["13:00", "15:30", "18:00"], "../img/asiatica1.jpg", [6, 7, 9, 10, 5]);
+        it('Si se reserva un horario sin pasar parámetro a la función, el arreglo se mantiene igual.', () => {
+            const nuevoRestaurant = new Restaurant();
+            nuevoRestaurant.horarios = ["13:00", "15:30", "18:00"];
             nuevoRestaurant.reservarHorario();
             expect(nuevoRestaurant.horarios).to.eql(["13:00",'15:30','18:00']);
         })
     })
 
     describe('obtenerPuntacion()', () => {
-        it('Dado un restaurant con determinadas calificaciones, la puntuación (que es el promedio de ellas) se calcula correctamente.', () => {
-            const nuevoRestaurant = new Restaurant(1, "TAO Uptown", "Asiática", "Nueva York", ["13:00", "15:30", "18:00"], "../img/asiatica1.jpg", [6, 7, 9, 10, 5]);
+        it('Si un restaurant tiene calificaciones, la puntuación se calcula correctamente como el promedio de ellas.', () => {
+            const nuevoRestaurant = new Restaurant();
+            nuevoRestaurant.calificaciones = [6, 7, 9, 10, 5];
             expect(nuevoRestaurant.obtenerPuntuacion()).to.equal(7.4);
         })
 
-        it('Dado un restaurant que no tiene ninguna calificación, la puntuación es igual a 0.', () => {
-            const nuevoRestaurant = new Restaurant(1, "TAO Uptown", "Asiática", "Nueva York", ["13:00", "15:30", "18:00"], "../img/asiatica1.jpg", []);
+        it('Si un restaurant no tiene ninguna calificación, la puntuación es igual a 0.', () => {
+            const nuevoRestaurant = new Restaurant();
+            nuevoRestaurant.calificaciones = [];
             expect(nuevoRestaurant.obtenerPuntuacion()).to.equal(0);
         })
     })
 
     describe('calificar()', () => {
-        it('Al agregar una nueva calificación válida, la misma debe agregarse al arreglo calificaciones.', () => {
-            const nuevoRestaurant = new Restaurant(1, "TAO Uptown", "Asiática", "Nueva York", ["13:00", "15:30", "18:00"], "../img/asiatica1.jpg", [6, 7, 9, 10, 5]);
+        it('Si la nueva calificación es válida, debe agregarse al arreglo calificaciones.', () => {
+            const nuevoRestaurant = new Restaurant();
+            nuevoRestaurant.calificaciones = [6, 7, 9, 10, 5];
             nuevoRestaurant.calificar(7);
             expect(nuevoRestaurant.calificaciones).to.eql([6, 7, 9, 10, 5, 7]);
         })
 
-        it('Si la nueva calificación es menor a 0 no debe agregarse al arreglo calificaciones.', () => {
-            const nuevoRestaurant = new Restaurant(1, "TAO Uptown", "Asiática", "Nueva York", ["13:00", "15:30", "18:00"], "../img/asiatica1.jpg", [6, 7, 9, 10, 5]);
+        it('Si la nueva calificación es menor a 0, no debe agregarse al arreglo calificaciones.', () => {
+            const nuevoRestaurant = new Restaurant();
+            nuevoRestaurant.calificaciones = [6, 7, 9, 10, 5];
             nuevoRestaurant.calificar(-1);
             expect(nuevoRestaurant.calificaciones).to.eql([6, 7, 9, 10, 5]);
         })
 
-        it('Si la nueva calificación es mayor a 10 no debe agregarse al arreglo calificaciones.', () => {
-            const nuevoRestaurant = new Restaurant(1, "TAO Uptown", "Asiática", "Nueva York", ["13:00", "15:30", "18:00"], "../img/asiatica1.jpg", [6, 7, 9, 10, 5]);
+        it('Si la nueva calificación es mayor a 10, no debe agregarse al arreglo calificaciones.', () => {
+            const nuevoRestaurant = new Restaurant();
+            nuevoRestaurant.calificaciones = [6, 7, 9, 10, 5];
             nuevoRestaurant.calificar(11);
             expect(nuevoRestaurant.calificaciones).to.eql([6, 7, 9, 10, 5]);
         })
 
-        it('Si la nueva calificación no es un número entero no debe agregarse al arreglo calificaciones.', () => {
-            const nuevoRestaurant = new Restaurant(1, "TAO Uptown", "Asiática", "Nueva York", ["13:00", "15:30", "18:00"], "../img/asiatica1.jpg", [6, 7, 9, 10, 5]);
+        it('Si la nueva calificación no es un número entero, no debe agregarse al arreglo calificaciones.', () => {
+            const nuevoRestaurant = new Restaurant();
+            nuevoRestaurant.calificaciones = [6, 7, 9, 10, 5];
             nuevoRestaurant.calificar(4.5);
             expect(nuevoRestaurant.calificaciones).to.eql([6, 7, 9, 10, 5]);
         })
@@ -64,56 +73,56 @@ describe('Restaurant', () => {
 
 describe('Listado', () => {
     describe('buscarRestaurante(id)', () => {
-        it('Se debe regresar el restaurant buscado si existe el número de id.', () => {
+        it('Si existe el número de id del restaurant buscado, se debe regresar el restaurant buscado.', () => {
            const restaurantBuscado = listado.buscarRestaurante(1);
            const restaurantIdUno = listado.restaurantes[0];
            expect(restaurantBuscado).to.equal(restaurantIdUno);
 
         })
 
-        it('Se debe mostrar un alerta si no existe el número de id del restaurant buscado.', () => {
+        it('Si no existe el número de id del restaurant buscado, se debe mostrar un alerta ', () => {
             const restaurantBuscado = listado.buscarRestaurante(30);
             expect(restaurantBuscado).to.equal('No se ha encontrado ningún restaurant');
         })
 
-        it('Se debe mostrar un alerta si no se ingresa un id como parámetro.', () => {
+        it('Si no se ingresa un id como parámetro, se debe mostrar un alerta.', () => {
             const restaurantBuscado = listado.buscarRestaurante(0);
             expect(restaurantBuscado).to.equal('No se ha encontrado ningún restaurant');
         })
     })
 
     describe('obtenerRestaurantes()', () => {
-        it('Si se filtra por Rubro se deberá crear un nuevo Array solo con los restaurants de ese Rubro', () => {
+        it('Si se filtra por Rubro, se debe crear un nuevo Array solo con los restaurants de ese Rubro', () => {
             const nuevoListado = new Listado(listadoDeRestaurantes);
             const restaurentesPorRubro = nuevoListado.obtenerRestaurantes('Pizza', null, null);
             expect(restaurentesPorRubro.length).to.equal(4);
         })
 
-        it('Si se filtra por Ciudad se deberá crear un nuevo Array solo con los restaurants de esa Ciudad', () => {
+        it('Si se filtra por Ciudad, se debe crear un nuevo Array solo con los restaurants de esa Ciudad', () => {
             const nuevoListado = new Listado(listadoDeRestaurantes);
             const restaurentesPorRubro = nuevoListado.obtenerRestaurantes(null,'París', null);
             expect(restaurentesPorRubro.length).to.equal(6);
         })
 
-        it('Si se filtra por Horario se deberá crear un nuevo Array solo con los restaurants que se pueden reservar en ese horario', () => {
+        it('Si se filtra por Horario, se debe crear un nuevo Array solo con los restaurants que se pueden reservar en ese horario', () => {
             const nuevoListado = new Listado(listadoDeRestaurantes);
             const restaurentesPorRubro = nuevoListado.obtenerRestaurantes(null, null, '20:30');
             expect(restaurentesPorRubro.length).to.equal(1);
         })
 
-        it('Si se filtra por Rubro, Ciudad y Horario se deberá crear un nuevo Array solo con los restaurants que cumplan con los 3 requisitos', () => {
+        it('Si se filtra por Rubro, Ciudad y Horario, se debe crear un nuevo Array solo con los restaurants que cumplan con los 3 requisitos', () => {
             const nuevoListado = new Listado(listadoDeRestaurantes);
             const restaurentesPorRubro = nuevoListado.obtenerRestaurantes('Pizza', 'París', '12:00');
             expect(restaurentesPorRubro.length).to.equal(1);
         })
 
-        it('Si no se ingresa ningún filtro como parámetro no se genera un nuevo array filtrado', () => {
+        it('Si no se ingresa ningún filtro como parámetro, no se genera un nuevo array filtrado, sino que será igual al array restaurantes', () => {
             const nuevoListado = new Listado(listadoDeRestaurantes);
             const restaurentesPorRubro = nuevoListado.obtenerRestaurantes( null, null, null);
             expect(restaurentesPorRubro).to.eql(nuevoListado.restaurantes);
         })
 
-        it('Si se ingresan parámetros que no coinciden con las propiedades Rubro, Ciudad u Horario no se genera un nuevo array filtrado', () => {
+        it('Si se ingresan parámetros que no coinciden con las propiedades Rubro, Ciudad u Horario, el array filtrado quedará vacío', () => {
             const nuevoListado = new Listado(listadoDeRestaurantes);
             const restaurentesPorRubro = nuevoListado.obtenerRestaurantes( 'Tacos', 'La Lupita', '04:00');
             expect(restaurentesPorRubro.length).to.equal(0);
